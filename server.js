7f13d5bee85ace9a532f0b1318b2fb8d1edb7a70
MINOT Maxime <maxime.minot@emea.msad.sopra>
2019-06-26 11:39:03 +0200

    Correction bug prod

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// server.js
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/{{your-app-name}}'));
+// Run the app by serving the static files
+// in the dist directory
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
-});
+app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default
+// Heroku port
+
 app.listen(process.env.PORT || 8080);
